03c271a694e89fbf56667791d87d632b89235f9c
Suryansh9369 <suryanshv16@gmail.com>
2026-06-27 09:29:20 +0000

    debug for model trainer

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ import numpy as np
 import pandas as pd
 import pickle
 
+from sklearn.metrics import recall_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import customException
 
 def save_object(file_path, obj):
@@ -17,5 +20,33 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
         
+    except Exception as e:
+        raise customException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report={}
+        
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+            
+            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            train_model_score=recall_score(y_train, y_train_pred)
+            test_model_score=recall_score(y_test, y_test_pred)
+            
+            report[list(models.keys())[i]]=test_model_score
+            
+        return report
+    
     except Exception as e:
         raise customException(e, sys)
