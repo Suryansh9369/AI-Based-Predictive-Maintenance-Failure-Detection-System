41528d41ba2664fdcd2d7a44c81d7d9a93f4a510
Suryansh9369 <suryanshv16@gmail.com>
2026-06-27 09:28:49 +0000

    modified some steps and debug

---
+++ src/components/model_trainer.py
@@ -0,0 +1,103 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix, make_scorer, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, average_precision_score
+
+from src.exception import customException
+from src.logger import logging
+
+from src.utils import save_object, evaluate_models
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifcats", "model.pkl")
+    
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+        
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("split training and test input data.")
+            
+            X_train, y_train, X_test, y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            
+            models = {
+                "Logistic Regression": LogisticRegression(),
+                "Decision Tree Classifier": DecisionTreeClassifier(),
+                "Random Forest Classifier": RandomForestClassifier(),
+                "XGBClassifier": XGBClassifier(),
+                "Support Vector": svm.SVC(),
+                "GaussianNB": GaussianNB()
+            }
+            param = {
+                "Logistic Regression": {},
+
+                "Decision Tree Classifier": {
+                    "criterion": ["gini", "entropy"],
+                    "max_depth": [5, 10, None]
+                },
+
+                "Random Forest Classifier": {
+                    "n_estimators": [100, 200],
+                    "max_depth": [10, 20]
+                },
+
+                "XGBClassifier": {
+                    "n_estimators": [100, 200],
+                    "learning_rate": [0.01, 0.1]
+                },
+
+                "Support Vector": {
+                    "C": [0.1, 1, 10],
+                    "kernel": ["linear", "rbf"]
+                },
+
+                "GaussianNB": {}
+            }
+            
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test, y_test=y_test, models=models, param=param)
+            
+            ## To get best best model score from dict
+            best_model_score = max(list(model_report.values()))
+            
+            ## to get best model name from dict
+            
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model=models[best_model_name]
+            
+            if best_model_score<0.6:
+                raise customException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            
+            predicted=best_model.predict(X_test)
+            
+            recall_scored = recall_score(y_test, predicted)
+            
+            return recall_scored
+            
+            
+        except Exception as e:
+            raise customException(e,sys)
